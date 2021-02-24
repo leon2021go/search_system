25cc5f7def86ff2e95e2940c08b3e97e332e3441
mengzhua2020 <petmon@163.com>
2021-02-25 01:53:04 +0800

    全局错误码定义

--- src/main/java/com/leon/flying/common/GlobelExceptionHandler.java
+++ src/main/java/com/leon/flying/common/GlobelExceptionHandler.java
@@ -3,6 +3,7 @@ package com.leon.flying.common;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +16,8 @@ public class GlobelExceptionHandler {
     public MzRespose doError(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Exception e){
         if(e instanceof MzBusinessException){
             return MzRespose.error(-1, e.getMessage());
+        }else if(e instanceof NoHandlerFoundException){
+            return MzRespose.error(-1, "找不到执行路径");
         }else{
             return MzRespose.internalError();
         }
